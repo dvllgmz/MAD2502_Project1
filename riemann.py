360b2382a6ec5d2b4d09969a1d4675d851c3afe5
Daniela <dvillagomez@ufl.edu>
2025-02-07 12:15:30 -0500

    Initial project formatting

---
+++ riemann.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def left_endpoint(x_vals: np.ndarray, func: np.ufunc):
+    # body
+
+def trapezoid(x_vals: np.ndarray, func: np.ufunc):
+    # body
+
+def simpson(x_vals: np.ndarray, func: np.ufunc):
+    # body
+
