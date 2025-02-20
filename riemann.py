adc6fe522c54027b923a2b4720b130616da37abb
Thomas Barbato <t.barbato@ufl.edu>
2025-02-19 23:49:33 -0500

    Part 2, not sure I'm understanding Simpson's method right

--- riemann.py
+++ riemann.py
@@ -1,17 +1,18 @@
 import numpy as np
 
 def left_endpoint(x_vals: np.ndarray, func: np.ufunc):
-    sum = 0
-    for i in range(x_vals.shape[0] - 1):
-        sum += func(x_vals[i]) * (x_vals[i + 1] - x_vals[i])
-    return sum
+    lefts = x_vals[:-1]
+    rights = x_vals[1:]
+    rects = (rights - lefts) * func(lefts)
+    return sum(rects)
 
 def trapezoid(x_vals: np.ndarray, func: np.ufunc):
-    sum = 0
-    for i in range(x_vals.shape[0] - 1):
-        sum += ((func(x_vals[i]) + func(x_vals[i + 1])) / 2.0) * (x_vals[i + 1] - x_vals[i])
-    return sum
+    left_x = x_vals[:-1]
+    right_x = x_vals[1:]
+    left_y = func(left_x)
+    right_y = func(right_x)
+    traps = (right_x - left_x) * ((left_y + right_y) / 2)
+    return sum(traps)
 
 def simpson(x_vals: np.ndarray, func: np.ufunc):
-    # body
-
+    return ((x_vals[-1] - x_vals[0]) / 6.0) * (func(x_vals[0]) + 4 * func((x_vals[0] + x_vals[-1]) / 2.0) + func(x_vals[-1]))
