b09aaaf9507a0c6ddbd6cabd2bc19c6a5280610a
Thomas Barbato <t.barbato@ufl.edu>
2025-02-19 21:37:12 -0500

    Completed left_endpoint() and trapezoid()

--- riemann.py
+++ riemann.py
@@ -1,10 +1,16 @@
 import numpy as np
 
 def left_endpoint(x_vals: np.ndarray, func: np.ufunc):
-    # body
+    sum = 0
+    for i in range(x_vals.shape[0] - 1):
+        sum += func(x_vals[i]) * (x_vals[i + 1] - x_vals[i])
+    return sum
 
 def trapezoid(x_vals: np.ndarray, func: np.ufunc):
-    # body
+    sum = 0
+    for i in range(x_vals.shape[0] - 1):
+        sum += ((func(x_vals[i]) + func(x_vals[i + 1])) / 2.0) * (x_vals[i + 1] - x_vals[i])
+    return sum
 
 def simpson(x_vals: np.ndarray, func: np.ufunc):
     # body
