1c076039226c58002b6ad0ac8768d6d09ff4d698
Mayteetad <mpukdeetho@ufl.edu>
2025-02-20 19:49:39 -0500

    get_cartisian_coords & is_inside_triangle added.

--- barycentric.py
+++ barycentric.py
@@ -1,12 +1,6 @@
-from matplotlib.patches import Polygon
-import matplotlib.pyplot as plt
 import numpy as np
 
-"""
-MAKE SURE YOU ADD MATPLOTLIB TO YOUR INTERPETERS 
-TOP RIGHT GEAR -> PROJECT -> PROJECT INTERPERTER
 
-"""
 def get_barycentric_coordinates(triangle_coordinates, point_coordinates):
 
     #setting up triangle and "goal" coordinates.
@@ -26,8 +20,7 @@ def get_barycentric_coordinates(triangle_coordinates, point_coordinates):
     """
     goal:
     - eliminate variables until we have 1 left that we can solve for.
-    - then, plug in the rest until everything is complete
-    """
+    - then, plug in the rest until everything is complete"""
     #get rid of one variable via subsitution (r1 = 1- r2 -r3). Remeber this for when we solve for r1 after we solve for r2 and r3.
     #so now it looks like
     #(1-r2-r3)*x1+r2*x2+r3*x3 == x_goal
@@ -58,11 +51,12 @@ def get_barycentric_coordinates(triangle_coordinates, point_coordinates):
     # and multiply the second equation by a1...
     #(a1+a2) * r2 + (a1 + b2) * r3 = c2*a1
     # now we subtract the top by the bottom...
-    #(a2+a1-(a1+a2)) * r2 + (a2+b1 - (a1+b2)) * r3= c1*a2 - c2*a1
+    #(a2+a1-(a1+a2)) * r2 + (a2*b1 - (a1*b2)) * r3= c1*a2 - c2*a1
     # ^^^^^^^^^^^^^ this term is 0 now, so now we can solve for r3.
     # r3 = (c1*a2-c2*a1)/((a2+b1-(a1+b2)))
+    #
     r3_num = c1*a2 - c2*a1
-    r3_dem = a2+b1 - (a1+b2)
+    r3_dem = a2*b1 - (a1*b2)
     r3 = r3_num/r3_dem
     # now we can plug in r3 into the equations we saved earlier. here are they for reference again.
 
@@ -75,11 +69,11 @@ def get_barycentric_coordinates(triangle_coordinates, point_coordinates):
     # r2 = (c2 - (b2*r3))/a2.
     # make sure they equal eachother (with all the variables, if not then raise an error)
     r2_num_1 = c1-(b1*r3)
-    r2_num_2 = c2-(a2*r3)
+    r2_num_2 = c2-(b2*r3)
 
     r2_value_1 = np.round(r2_num_1/a1, decimals=3)
     r2_value_2 = np.round(r2_num_2/a2, decimals=3)
-
+    #we use np.round() because floating point stuff is funny. you know with the 2.00000001 and all of that. theres probably a better way to do this?
     # since we know r2 and r3 now, we can go back all the way to here:
     #r1 = 1- r2 -r3
     if r2_value_1 == r2_value_2:
@@ -91,6 +85,51 @@ def get_barycentric_coordinates(triangle_coordinates, point_coordinates):
 
     # and then we should be good?
     return np.array([r1, r2, r3])
+
+
+
+def get_cartesian_coordinates(triangle_coordinates, barycentric_coordinates):
+    """
+    Okay, so now we have:
+    r1 * x1 + r2 * x2 + r3 * x3 = x_goal
+    r1 * y1 + r2 * y2 + r3 * y3 = y_goal
+    r1 + r2 + r3 = 1, but we know r1,r2,r3 (barycentric_coordinates) and everything that isn't x_goal and y_goal. (triangle coordinates)
+    :param triangle_coordinates:
+    :param barycentric_coordinates:
+    :return:
+    """
+    # this is just multiplication? Lets set up our variables first
+    x1 = triangle_coordinates[0,0]
+    y1 = triangle_coordinates[1,0]
+    x2 = triangle_coordinates[0,1]
+    y2 = triangle_coordinates[1,1]
+    x3 = triangle_coordinates[0,2]
+    y3 = triangle_coordinates[1,2]
+    r1 = barycentric_coordinates[0]
+    r2 = barycentric_coordinates[1]
+    r3 = barycentric_coordinates[2]
+    # first, lets make sure r1, r2, r3 are valid.
+    if ((r1 + r2 + r3)) != 1:
+        return "invalid barycentric coordinates"
+    # compute like equations above
+    x_goal = r1 * x1 + r2 * x2 + r3 * x3
+    y_goal = r1 * y1 + r2 * y2 + r3 * y3
+    possible_result = np.array([x_goal, y_goal])
+    # another check to make sure we're not failing is to put it into the get_barycentric_coordinates, since they're sort of inverses of eachother.
+    checker = get_barycentric_coordinates(triangle_coordinates, possible_result)
+
+    if (checker[0], checker[1], checker[2]) != (r1,r2,r3):
+        return "something went wrong, cartesian"
+    return possible_result
+
+def is_inside_triangle(triangle_coordinates, baycentric_coordinates):
+    """* The function `is_inside_triangle` takes the same input arguments as `get_barycentric_coordinates` but it returns a `bool` as to whether the point lies inside of the triangle or not.
+The relevant property of barycentric coordinates is that a point is inside of the the triangle if all coordinates are non-negative.
+    So we just check if any of the provided array is less than 1, i think? Unless theres a misunderstanding. If anything, get the 3-figure array, check if they're negative. if they are, return false. if not, return true.
+    """
+    if ((baycentric_coordinates[0] < 0) or (baycentric_coordinates[1] < 0) or  (baycentric_coordinates[2] < 0)):
+        return False
+    return True
 
 def get_barycentric_coordinates(triangle_coordinates, point_coordinates):
     x1, x2, x3 = triangle_coordinates[0]
